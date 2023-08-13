316efa47094e4d7007a028ca2235ee11b4345c0b
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-08-13 14:58:07 +1000

    Navbar Styles Retweaked

---
+++ src/Components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <section id="hero" className="hero">
+      <div className="hero-content">
+        <h1>Welcome to our Bar</h1>
+        <p>
+          Discover a place where everyone is welcome to share their stories!
+        </p>
+        <button className="cta-button">Explore Upcoming Events</button>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,14 +9,17 @@ function Navbar() {
   };
 
   return (
-    <nav>
-      <ul className="nav-links">
-        <li onClick={() => scrollToSection('about')}>About</li>
-        <li onClick={() => scrollToSection('events')}>Events</li>
-        <li onClick={() => scrollToSection('bookings')}>Bookings</li>
-        <li onClick={() => scrollToSection('contact')}>Contact</li>
-      </ul>
-    </nav>
+    <header className="Navbar-Component-Container">
+      <h1>Market Bar</h1>
+      <nav>
+        <ul className="nav-links">
+          <li onClick={() => scrollToSection('about')}>About</li>
+          <li onClick={() => scrollToSection('events')}>Events</li>
+          <li onClick={() => scrollToSection('bookings')}>Bookings</li>
+          <li onClick={() => scrollToSection('contact')}>Contact</li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
